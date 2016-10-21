0ca11553083523b5774b5f89a470aaa0500e3b93
Nikolai Bravilov <bravilov@mail.ru>
2016-10-21 11:21:07 +0500

    Начальный коммит

---
+++ Homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Homework3.StateFluentBuilder;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letter = new LetterBuilder()
+                .AddRecipient("Niko")
+                .AddLetterBody("Hello, Niko")
+                .Build();
+            Console.WriteLine(letter);
+            Console.WriteLine();
+            var letter2 = new LetterBuilder()
+                .AddRecipient("Niko")
+                .AddLetterBody("Hello, Niko")
+                .AddCopyRecipient("Niko2")
+                .AddTopic("Hello")
+                .AddCopyRecipient("Niko3")
+                .Build();
+            Console.WriteLine(letter2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework3/StateFluentBuilder/LetterBuilder.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+
+namespace Homework3.StateFluentBuilder
+{
+    public class LetterBuilder
+    {
+        public LetterBodyBuilder AddRecipient(string recipient)
+        {
+            return new LetterBodyBuilder(recipient);
+        }
+
+        public class LetterBodyBuilder
+        {
+            private readonly string _recipient;
+
+            public LetterBodyBuilder(string recipient)
+            {
+                _recipient = recipient;
+            }
+
+            public FinalLetterWithoutTopicBuilder AddLetterBody(string body)
+            {
+                return new FinalLetterWithoutTopicBuilder(_recipient, body);
+            }
+        }
+
+        public class FinalLetterWithoutTopicBuilder
+        {
+            private readonly string _recipient;
+            private readonly string _body;
+            private readonly List<string> _copyRecipients;
+
+            public FinalLetterWithoutTopicBuilder(string recipient, string body)
+            {
+                _recipient = recipient;
+                _body = body;
+                _copyRecipients = new List<string>();
+            }
+
+            public FinalLetterWithoutTopicBuilder AddCopyRecipient(string recipient)
+            {
+                _copyRecipients.Add(recipient);
+                return this;
+            }
+
+            public FinalLetterBuilder AddTopic(string topic)
+            {
+                return new FinalLetterBuilder(_recipient, _body, _copyRecipients, topic);
+            }
+
+            public string Build()
+            {
+                string result = "";
+                result += "Получатель: " + _recipient + "\n";
+                if (_copyRecipients.Count != 0)
+                {
+                    result += "Отправить копию: ";
+                    foreach (string recipient in _copyRecipients)
+                    {
+                        result += recipient + "; ";
+                    }
+                    result += "\n";
+                }
+                result += _body;
+                return result;
+            }
+        }
+
+        public class FinalLetterBuilder
+        {
+            private readonly string _recipient;
+            private readonly string _body;
+            private readonly List<string> _copyRecipients;
+            private readonly string _topic;
+
+            public FinalLetterBuilder(string recipient, string body, List<string> copyRecipients, string topic)
+            {
+                _recipient = recipient;
+                _body = body;
+                _copyRecipients = copyRecipients;
+                _topic = topic;
+            }
+
+            public FinalLetterBuilder AddCopyRecipient(string recipient)
+            {
+                _copyRecipients.Add(recipient);
+                return this;
+            }
+
+            public string Build()
+            {
+                string result = "";
+                result += "Получатель: " + _recipient + "\n";
+                if (_copyRecipients.Count != 0)
+                {
+                    result += "Отправить копию: ";
+                    foreach (string recipient in _copyRecipients)
+                    {
+                        result += recipient + "; ";
+                    }
+                    result += "\n";
+                }
+                if (!_topic.Equals(""))
+                {
+                    result += _topic + "\n";
+                }
+
+                result += _body;
+                return result;
+            }
+        }
+
+    }
+}
